8d6017988159ca07d40ca0e53e986c36511a847d
GitHub Action <action@github.com>
2020-07-27 11:55:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sililestone/ideaplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sililestone.ideaplugintest.services
+
+import com.github.sililestone.ideaplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
